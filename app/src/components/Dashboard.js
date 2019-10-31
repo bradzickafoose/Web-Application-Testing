a18716f83020d8a240a5fcab774a42fde5360223
Brad <brad@viziworx.com>
2019-10-31 16:27:29 -0400

    Setup function base

---
+++ app/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Dashboard() {
+	return (
+		<div className='dashboard'>
+			<div>Strike</div>
+			<div>Ball</div>
+			<div>Foul</div>
+			<div>Hit</div>
+		</div>
+	);
+}
